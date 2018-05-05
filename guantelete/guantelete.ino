97c34a1cc7302de9ec5a7c717dc2a2c3101390e2
Diego Dorado <diegodorado@gmail.com>
2018-05-05 13:06:04 -0300

    updates project to work with a 5 i/o attiny85

--- guantelete/guantelete.ino
+++ guantelete/guantelete.ino
@@ -3,19 +3,19 @@
   Cortito y al pie:
 
   
-                         pin4:   G2 --------  `+:/.                    
-                                     ``       :. `+   `+::  ----- pin5: A2         
-                  pin3:   F2 -----  .o.o.     /  `+   +  /             
+                         pin2:   G2 --------  `+:/.                    
+                                     ``       :. `+   `+::  ----- pin3: A2         
+                  pin1:   F2 -----  .o.o.     /  `+   +  /             
                                     .s  o`    o  `/   o  +             
                                      d` ./    s  .-  -- `/             
                                      -o  :-  ./  -- `+  :.   -/+.      
-                                      y   +` --  `+ +`  o   `/ :-   ----- pin6: C3
+                                      y   +` --  `+ +`  o   `/ :-   ----- pin4: C3
                                       /o   / `:  `+ /   /  ./ `+       
                                        s:  ././  `o:.  :. /- `+        
                                         h`  ./.   :/   o +. .+         
                                         +-             -:. `+          
                            -+/--:/.     /`                `o           
-          pin2:  D2 -----   ./:   -/    /-                 +           
+            GND:  --------   ./:   -/    /-                 +           
                                ::` `:-  s`                `/           
                                  //  `:/o`                /`           
                                   ::                      s            
@@ -26,37 +26,38 @@
                                           :.           /`              
 */
 
+#include "SendOnlySoftwareSerial.h"
 
-int pines[] = {2,3,4,5,6};
-int estadoPines[] = {1,1,1,1,1};
-int antirebotes[] = {0,0,0,0,0};
+SendOnlySoftwareSerial midiSerial(0);
+
+int pines[] = {1,2,3,4};
+int estadoPines[] = {1,1,1,1};
+int antirebotes[] = {0,0,0,0};
 
 /*
 Notas a MIDI:
 -------------
-    D2   38
     F2   41
     G2   43
     A2   45  ( 110hz)
     C3   48
 
 */
-int notas[] = {38,41,43,45,48};
-
+int notas[] = {41,43,45,48};
 
 void setup() {
 
-  for(int i=0;i<5;i++){
+  for(int i=0;i<4;i++){
     pinMode(pines[i], INPUT_PULLUP);
   }
   
-  Serial.begin(9600);
+  midiSerial.begin(31250);
 
 }
 
 void loop() {
-
-  for(int i=0;i<5;i++){
+  
+  for(int i=0;i<4;i++){
     int nuevoEstadoPin = digitalRead(pines[i]);
     
     if (estadoPines[i] != nuevoEstadoPin ){
@@ -87,9 +88,9 @@ void cambiarEstadoPin(int pin, int nuevoEstado) {
 
 //  plays a MIDI note.
 void noteOn(int pitch, int velocity) {
-  Serial.write(0x90);
-  Serial.write(pitch);
-  Serial.write(velocity);
+  midiSerial.write(0x90);
+  midiSerial.write(pitch);
+  midiSerial.write(velocity);
 }
 
 void noteOff(int pitch) {
